620480902f97810a19526f5e61b25d2e4f12bf1c
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-08-29 18:13:01 +0200

    chore: add vite svelte example

---
+++ examples/vite-svelte/svelte.config.ts
@@ -0,0 +1,8 @@
+import sveltePreprocess from 'svelte-preprocess'
+
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+
+}
---
+++ examples/vite-svelte/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import AutoImport from '../../src/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    AutoImport({
+      imports: [
+        'svelte',
+        'svelte/store',
+        'svelte/transition',
+      ],
+      include: [/\.svelte/],
+    }),
+  ],
+})
