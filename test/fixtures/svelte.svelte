6e45620794cda781441f13b4085a950050956e00
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-08-29 16:26:41 +0200

    test: add svelte test for transform

---
+++ test/fixtures/svelte.svelte
@@ -0,0 +1,18 @@
+const list = writable([1, 2, 3])
+const size = tweened(1, {
+  duration: 300,
+  easing: cubicOut
+});
+onMount(() => {})
+
+{#each $list as n (n)}
+<div animate:flip="{{delay: 250, duration: 250, easing: quintOut}}">
+  {n}
+</div>
+{/each}
+
+{#if condition}
+  <div transition:fade="{{amount: 10}}">
+    fades in and out
+  </div>
+{/if}
