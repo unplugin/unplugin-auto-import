d25d1623540da0603a94be1b4bb74dab7fb79b76
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-08-29 18:12:10 +0200

    chore: add vite react example

---
+++ examples/vite-react/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import AutoImport from '../../src/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    reactRefresh(),
+    AutoImport({
+      imports: 'react',
+    }),
+  ],
+})
