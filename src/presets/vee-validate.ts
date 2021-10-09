3844e973599da5b0dd5f7a70df50674e4297d5ce
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-10-09 15:17:05 +0100

    create vee-validate preset

---
+++ src/presets/vee-validate.ts
@@ -0,0 +1,35 @@
+import { ImportsMap } from '../types'
+
+export default <ImportsMap>({
+  'vee-validate': [
+    // https://github.com/logaretm/vee-validate/blob/main/packages/vee-validate/src/index.ts
+    'validate',
+    'defineRule',
+    'configure',
+    'Field',
+    'Form',
+    'FieldArray',
+    'ErrorMessage',
+    'useField',
+    'useForm',
+    'useFieldArray',
+    'useResetForm',
+    'useIsFieldDirty',
+    'useIsFieldTouched',
+    'useIsFieldValid',
+    'useIsSubmitting',
+    'useValidateField',
+    'useIsFormDirty',
+    'useIsFormTouched',
+    'useIsFormValid',
+    'useValidateForm',
+    'useSubmitCount',
+    'useFieldValue',
+    'useFormValues',
+    'useFormErrors',
+    'useFieldError',
+    'useSubmitForm',
+    'FormContextKey',
+    'FieldContextKey',
+  ],
+})
