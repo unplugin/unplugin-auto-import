d20b627d3631cbebf4f66f790c5502e6a99d8bb9
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-10-11 00:17:13 +0200

    chore: adjust type entries on composition api types

--- src/global-types/vue.ts
+++ src/global-types/vue.ts
@@ -1,30 +1,39 @@
 import { ImportsTypeMap, ImportNameAlias, ImportNameTypeAlias } from '../types'
 
 export const CommonCompositionTypes: (string | ImportNameAlias | ImportNameTypeAlias)[] = [
-  [true, 'Ref'],
-  [true, 'ComputedRef'],
-  [true, 'WritableComputedRef'],
-  [true, 'WritableComputedOptions'],
-  [true, 'ToRef'],
-  [true, 'ToRefs'],
-  [true, 'UnwrapRef'],
-  [true, 'ShallowUnwrapRef'],
-  [true, 'DeepReadonly'],
-  [true, 'InjectionKey'],
+  ['Ref', true],
+  ['ComputedRef', true],
+  ['WritableComputedRef', true],
+  ['WritableComputedOptions', true],
+  ['ToRef', true],
+  ['ToRefs', true],
+  ['UnwrapRef', true],
+  ['ShallowUnwrapRef', true],
+  ['DeepReadonly', true],
+  ['InjectionKey', true],
   'WatchEffect',
-  [true, 'WatchSource'],
-  ['V, OV', 'WatchCallback'],
-  ['Immediate', 'WatchOptions'],
+  ['WatchSource', true],
+  ['WatchCallback', 'V, OV'],
+  ['WatchOptions', 'Immediate'],
   'WatchStopHandle',
   'ObjectEmitsOptions',
   'EmitsOptions',
 ]
 
+export const pickVueTypes = (...types: Array<string>): (string | ImportNameAlias | ImportNameTypeAlias)[] => {
+  const result: (string | ImportNameAlias | ImportNameTypeAlias)[] = []
+  const set = new Set<string>(types)
+  CommonCompositionTypes.forEach((type) => {
+    if (typeof type === 'string')
+      set.has(type) && result.push(type)
+    else if (set.has(type[0]))
+      result.push(type)
+  })
+  return result
+}
+
 export default <ImportsTypeMap>({
   vue: [
     ...CommonCompositionTypes,
-
-    // vue 3 only
-    'ReactiveEffectOptions',
   ],
 })
