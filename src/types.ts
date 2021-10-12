a4a9f527fb0ce6b730cc219df85831603a26e5a8
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-10-12 14:02:26 +0200

    chore: update `ImportNameTypeAlias` jsdocs

--- src/types.ts
+++ src/types.ts
@@ -9,9 +9,8 @@ import { TypePresetName } from './global-types'
 export type ImportNameAlias = [string, string]
 /**
  *  name => type parameters:
- *  - `false`: no paremetrized type
  *  - `true`: parametrized type `T`, for example when using `Ref<T>`
- *  - `custom`: parametrized type custom, for example when using `WatchCallback<V, OV>`, use `O, IV`
+ *  - `custom`: parametrized custom type, for example when using `WatchCallback<V, OV>`, use `O, IV`
  */
 export type ImportNameTypeAlias = [string, boolean | string]
 /**
