bed3689d84a45abc717ff9856186b1cd76145e95
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-11 16:06:25 +0800

    feat: support vueuse@8.0

--- src/presets/vueuse-core.ts
+++ src/presets/vueuse-core.ts
@@ -8,22 +8,29 @@ export default (): ImportsMap => {
   const excluded = ['toRefs', 'utils']
 
   if (!_cache) {
+    let indexesJson: any | undefined
     try {
+      const corePath = resolveModule('@vueuse/core') || process.cwd()
       const path = resolveModule('@vueuse/core/indexes.json')
-      const indexesJson = JSON.parse(readFileSync(path!, 'utf-8'))
-      _cache = {
-        '@vueuse/core': indexesJson
-          .functions
-          .filter((i: any) => ['core', 'shared'].includes(i.package))
-          .map((i: any) => i.name as string)
-          // only include functions with 4 characters or more
-          .filter((i: string) => i && i.length >= 4 && !excluded.includes(i)),
-      }
+        || resolveModule('@vueuse/metadata/index.json')
+        || resolveModule('@vueuse/metadata/index.json', { paths: [corePath] })
+      indexesJson = JSON.parse(readFileSync(path!, 'utf-8'))
     }
     catch (error) {
       console.error(error)
       throw new Error('[auto-import] failed to load @vueuse/core, have you installed it?')
     }
+    if (indexesJson) {
+      _cache = {
+        '@vueuse/core': indexesJson
+          .functions
+          .filter((i: any) => ['core', 'shared'].includes(i.package))
+          .map((i: any) => i.name as string)
+        // only include functions with 4 characters or more
+          .filter((i: string) => i && i.length >= 4 && !excluded.includes(i)),
+      }
+    }
   }
+
   return _cache || {}
 }
