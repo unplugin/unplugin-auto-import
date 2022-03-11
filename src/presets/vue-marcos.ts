2956dff957d94af775500cf7be9abdd13667d22f
Xwil <xw5427@gmail.com>
2022-03-11 17:22:19 +0800

    fix: vue/marcos -> vue/macros

--- src/presets/vue-marcos.ts
+++ src/presets/vue-marcos.ts
@@ -1,7 +1,7 @@
 import type { ImportsMap } from '../types'
 
 export default <ImportsMap>({
-  'vue/marcos': [
+  'vue/macros': [
     // https://vuejs.org/guide/extras/reactivity-transform.html#refs-vs-reactive-variables
     '$',
     '$$',
