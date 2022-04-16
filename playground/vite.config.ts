6d8377c448cc908185617abbc0b31c58663aceca
cjq <cjq02@qq.com>
2022-04-16 23:14:23 +0800

    feat: support importing defaults -- import defaultExport from 'module-name'(#162)

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     AutoImport({
       imports: ['vue', '@vueuse/core',
         {
-          dayjs: [['default', 'dayjs']],
+          dayjs: [['', 'dayjs']],
         },
       ],
       resolvers: [ElementPlusResolver()],
