8a5605945013afbf2edeb68747ed265d4d97254e
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-24 13:42:39 +0800

    docs: dirs auto import

--- playground/App.vue
+++ playground/App.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import HelloWorld from './HelloWorld.vue'
 ElMessage.warning('Test')
+const foo = useFoo()
 </script>
 
 <script lang="ts">
@@ -16,8 +17,8 @@ export default defineComponent({
 
 <template>
   <HelloWorld msg="hi" />
+  <pre>{{ foo }}</pre>
   <div v-loading="false">
-    123
     <ElButton>Hello</ElButton>
   </div>
 </template>
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
       resolvers: [
         ElementPlusResolver(),
       ],
+      dirs: [
+        './composables',
+      ],
     }),
   ],
 })
