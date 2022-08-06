62d467800092de7e5585961c8ca265c3824b917a
Mikkel RINGAUD <vexitofficial@gmail.com>
2022-08-06 17:53:27 +0200

    fix: add `@solidjs/router` alongside `solid-app-router` (#248)

---
+++ src/presets/solid-router.ts
@@ -0,0 +1,29 @@
+import type { ImportsMap } from '../types'
+
+export default <ImportsMap>({
+  '@solidjs/router': [
+    'Link',
+    'NavLink',
+    'Navigate',
+    'Outlet',
+    'Route',
+    'Router',
+    'Routes',
+    '_mergeSearchString',
+    'createIntegration',
+    'hashIntegration',
+    'normalizeIntegration',
+    'pathIntegration',
+    'staticIntegration',
+    'useHref',
+    'useIsRouting',
+    'useLocation',
+    'useMatch',
+    'useNavigate',
+    'useParams',
+    'useResolvedPath',
+    'useRouteData',
+    'useRoutes',
+    'useSearchParams',
+  ],
+})
