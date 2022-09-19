151b4bcaa1ba9bd29ddf905f00ec7e198e057fe7
Nebula <jsw0325@163.com>
2022-09-19 16:00:02 +0800

    feat: update vue-router preset

--- src/presets/vue-router.ts
+++ src/presets/vue-router.ts
@@ -2,7 +2,17 @@ import type { ImportsMap } from '../types'
 
 export default <ImportsMap>({
   'vue-router': [
+    'createRouter',
+    'createWebHistory',
+    'createMemoryHistory',
+    'createRouterMatcher',
+    'createWebHashHistory',
+
+    // composition api
     'useRouter',
     'useRoute',
+    'useLink',
+    'onBeforeRouteLeave',
+    'onBeforeRouteUpdate'
   ],
 })
