ae5637c3a314071d5cc434d24a14ecdc47b22243
Nebula <jsw0325@163.com>
2022-09-22 17:08:19 +0800

    Update vue-router.ts

--- src/presets/vue-router.ts
+++ src/presets/vue-router.ts
@@ -2,13 +2,6 @@ import type { ImportsMap } from '../types'
 
 export default <ImportsMap>({
   'vue-router': [
-    'createRouter',
-    'createWebHistory',
-    'createMemoryHistory',
-    'createRouterMatcher',
-    'createWebHashHistory',
-
-    // composition api
     'useRouter',
     'useRoute',
     'useLink',
