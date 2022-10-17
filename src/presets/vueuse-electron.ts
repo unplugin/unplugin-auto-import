f5f8a37140c598e01148d1e55fc9949bf60fd919
Michaël <contact@micode.fr>
2022-10-17 21:17:37 +0200

    Create vueuse-electron.ts

---
+++ src/presets/vueuse-electron.ts
@@ -0,0 +1,11 @@
+import type { ImportsMap } from '../types'
+
+export default <ImportsMap>({
+  '@vueuse/electron': [
+    'useIpcRenderer',
+    'useIpcRendererInvoke',
+    'useIpcRendererOn',
+    'useZoomFactor',
+    'useZoomLevel'
+  ],
+})
