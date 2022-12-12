3966271c13e31b4b00c90c8083c189d13beb28a5
倚窗听雨丿醉无心 <994039348@qq.com>
2022-12-12 20:05:09 +0800

    feat: add vue-router/composables preset

---
+++ src/presets/vue-router-composables.ts
@@ -0,0 +1,11 @@
+import type { ImportsMap } from '../types'
+
+export default <ImportsMap>({
+  'vue-router/composables': [
+    'useRouter',
+    'useRoute',
+    'useLink',
+    'onBeforeRouteLeave',
+    'onBeforeRouteUpdate',
+  ],
+})
