4cab123c212d38c7132fc6ca03bade347f94e533
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-09 00:38:32 +0100

    chore: update

--- playground/composables/foo.ts
+++ playground/composables/foo.ts
@@ -4,5 +4,5 @@ export function useFoo() {
 
 export const FOOBAR = 'auto import in Vue template'
 
-export const count: Ref<number> = ref(0)
+export const count = ref(0)
 export const doubled = computed(() => count.value * 2)
