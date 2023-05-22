dff2bf6a0a4369868997c584d2c2f67850a0d817
lishaobos <www.long126@qq.com>
2023-05-22 22:57:00 +0800

    fix: lint error

--- src/core/ctx.ts
+++ src/core/ctx.ts
@@ -166,13 +166,13 @@ ${dts}`.trim()}\n`
   async function scanDirs() {
     if (dirs?.length) {
       await unimport.modifyDynamicImports(async (imports) => {
-        const exports = await scanDirExports(dirs, {
+        const exports_ = await scanDirExports(dirs, {
           filePatterns: ['*.{tsx,jsx,ts,js,mjs,cjs,mts,cts}'],
         }) as ImportExtended[]
-        exports.forEach(i => i.__source = 'dir')
+        exports_.forEach(i => i.__source = 'dir')
         return modifyDefaultExportsAlias([
           ...imports.filter((i: ImportExtended) => i.__source !== 'dir'),
-          ...exports,
+          ...exports_,
         ], options)
       })
     }
