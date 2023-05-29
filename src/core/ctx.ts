fff2ef327acecebf184ee3a30cd07ca68604ef47
lishaobos <www.long126@qq.com>
2023-05-29 17:03:32 +0800

    fix: generate sourcemap always be high-resolution

--- src/core/ctx.ts
+++ src/core/ctx.ts
@@ -193,7 +193,7 @@ ${dts}`.trim()}\n`
 
     return {
       code: s.toString(),
-      map: s.generateMap({ source: id, includeContent: true, hires: options.sourcemapHires }),
+      map: s.generateMap({ source: id, includeContent: true, hires: true }),
     }
   }
 
