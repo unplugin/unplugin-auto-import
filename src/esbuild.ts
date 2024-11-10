d2cbaec7c43eadd31dfc7cf39b720bb5706f4e51
iiio2 <gomexxxx71@gmail.com>
2024-11-10 22:18:33 +0600

    chore: fix grammar

--- src/esbuild.ts
+++ src/esbuild.ts
@@ -1,5 +1,5 @@
 import type { Options } from './types'
 import unplugin from '.'
 
-// TODO: some upstream lib failed generate invalid dts, remove the any in the future
+// TODO: some upstream lib failed to generate invalid dts, remove the any in the future
 export default unplugin.esbuild as (options?: Options) => any
