1beb3e88a50907c643bb75d725e46489fbd92408
JounQin <admin@1stg.me>
2025-03-26 14:58:19 +0800

    fix: types fixed by upstream (#563)

--- src/rollup.ts
+++ src/rollup.ts
@@ -1,5 +1,3 @@
-import type { Options } from './types'
 import unplugin from '.'
 
-// TODO: some upstream lib failed to generate invalid dts, remove the any in the future
-export default unplugin.rollup as (options?: Options) => any
+export default unplugin.rollup
--- src/webpack.ts
+++ src/webpack.ts
@@ -1,5 +1,3 @@
-import type { Options } from './types'
 import unplugin from '.'
 
-// TODO: some upstream lib failed to generate invalid dts, remove the any in the future
-export default unplugin.webpack as (options?: Options) => any
+export default unplugin.webpack
