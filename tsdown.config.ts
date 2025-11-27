a9ead7007433c2356282bf7e9f0fbf589c53ac8c
Anthony Fu <github@antfu.me>
2025-11-27 11:16:26 +0900

    chore: update deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/*.ts'],
   format: ['esm', 'cjs'],
+  exports: true,
   dts: {
     resolve: ['@antfu/utils'],
   },
